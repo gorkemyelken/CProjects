db0f9750fd243b4261ee06d030224ab1a393bc3c
Görkem <gorkemyelken@gmail.com>
2020-12-23 01:07:44 +0300

    ads

---
+++ Homework4.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Count the number of characters in the string
+int counterChar(char str[])
+{
+ 	int i=0;
+ 	while(str[i] != '\0'){
+ 		i++;
+	}
+ 	return i;
+}
+
+// Count the number of words in the string
+int counterWord(char str[])
+{
+	int counter=0,countWord=1;
+ 	while(str[counter] != '\0'){
+ 		// Increase countWord by 1 in each space
+ 		if(str[counter] == ' '){
+ 			countWord++;
+		}
+ 		counter++;
+ 	}
+ 	return countWord;
+}
+
+int previousSpace(char str[],int startChar)
+{
+	while(str[startChar] != ' ' && startChar != '\0')
+	{
+		startChar--;
+	}
+	return startChar;
+}
+
+// Convert first character of each word to uppercase.
+void capitalized(char str[]){
+	// Capitalize the first character
+	if(str[0] >= 'a' && str[0]<='z'){
+ 			str[0] -= 32;
+ 			}
+ 	// Capitalize other words' initials
+	int counter = 1;
+ 	while(str[counter] != '\0'){
+ 		if(str[counter] == ' '){
+ 			if(str[counter+1] >= 'a' && str[counter+1]<='z'){
+ 			str[counter+1] -= 32;
+ 			}
+ 		}
+ 		counter++;
+ 	}
+ 	printf("%s",str);
+}
+
+// Reverse the order of words in the string in-place and display.
+void reverse(char str[])
+{
+	int i,counter=0,startPosition,finishPosition,countChar;
+	// Number of char
+	countChar = counterChar(str);
+	// Position of last character = Number of characters
+ 	finishPosition = countChar;
+ 	while(countChar >= 0 ){ 
+ 		// Last space
+		startPosition = previousSpace(str,countChar);
+ 		countChar = startPosition-1;
+ 		if(startPosition>0){
+ 			startPosition +=1;
+		}
+ 		// Output: Between start position and finish position
+ 		for(i=startPosition;i<finishPosition;i++){
+ 			printf("%c",str[i]);
+		}
+		// Space between words
+ 		printf(" ");
+ 		// New finish position
+ 		finishPosition = countChar + 1 ;
+}
+}
+
+// main function
+int main()
+{
+	printf("*****Word Processor Program*****\t\t\tBy Gorkem Yelken. Student ID:172010010032\n\n\n");
+	// Descriptions
+	printf("Decription for number of characters: It will count the number of characters in the string.\n\n");
+	printf("Decription for number of words: It will count the number of words in the string. The delimiter is a whitespace, ''  ''.\n\n");
+	printf("Decription for reverse: It will reverse the order of words in the string in-place and display.\n\n");
+	printf("Decription for capitalize: It will convert first character of each word to uppercase.\n\n");
+	// The string will be read from user input. The maximum string length is 250.
+ 	char str[250];
+ 	printf("String : ");
+ 	gets(str);
+ 	// Output: Number of characters
+ 	printf("Number of characters : %d\n",counterChar(str));
+ 	// Output: Number of words
+ 	printf("Number of words : %d\n",counterWord(str));
+ 	// Output: Reversed string
+ 	printf("Reversed : ");
+	reverse(str);
+	// Output: Capitalized string
+	printf("\nCapitalized : ");
+	capitalized(str);
+ 	return 0;
+}
---
+++ Homework5.c
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// atof function
+float my_atof(char *value)
+{
+    float result= 0.0;
+    int len = strlen(value);
+    int dotPosition = 0;
+	int i;
+    for (i = 0; i < len; i++)
+    {
+        if (value[i] == '.')
+        {
+          dotPosition = len - i  - 1;
+        }
+        else
+        {
+          result = result * 10.0 + (value[i]-'0');
+        }
+      }
+
+      while (dotPosition--)
+      {
+        result /= 10.0;
+      }
+
+    return result;
+}
+
+float calculate(float value1f, char operatorr, float value2f, unsigned short *flagptr)
+{
+
+    float result;
+    // addition
+    if(operatorr == '+')
+    {
+	result = value1f + value2f;
+    }
+    // subtraction
+    else if(operatorr == '-')
+    {
+	result = value1f - value2f;
+    }
+    // multiplication
+    else if(operatorr == '*')
+    {
+	result = value1f * value2f;
+    }
+    // division
+    else if(operatorr == '/')
+    {
+	if (value2f != 0)
+                    result = (float)value1f/value2f;
+                else
+                    {
+                       printf("Divide by zero error. ");
+                       *flagptr = 0;
+                    }
+    }
+    return result;
+}
+
+
+int main()
+{
+	char *value1;
+    char *value2;
+	float value1f,value2f;
+    char operatorr;
+    int k;
+    unsigned short flag = 1;   // flag which indicates any error in input
+ 	unsigned short *flagptr = &flag; // pointer to the above flag
+    printf("*****Calculator Program*****\t\t\tBy Gorkem Yelken. Student ID:172010010032\n\n\n");
+	// Descriptions
+	printf("Decription : It will take the characters in the string and do the math operations.\n\n");
+	// The string will be read from user input. The maximum string length is 250.
+	char operation[251];
+    printf("Operation : ");
+    gets(operation);
+    for (k = 0; k < strlen(operation); k++) { // check operator
+        if (operation[k] == '+' || operation[k] == '-' || operation[k] == '*' || operation[k] == '/' ) {
+        operatorr = operation[k];
+        break;
+        } 
+    }
+    if (operatorr == '+' || operatorr == '-' || operatorr == '*' || operatorr == '/' ) { // separate values
+    if(operation[k] == '+'){
+    	value1=strtok(operation,"+");
+    	value2=strtok(NULL,"+");
+	}
+    else if(operation[k] == '-'){
+    	value1=strtok(operation,"-");
+    	value2=strtok(NULL,"-");
+	}
+	else if(operation[k] == '*'){
+    	value1=strtok(operation,"*");
+    	value2=strtok(NULL,"*");
+	}
+	else if(operation[k] == '/'){
+    	value1=strtok(operation,"/");
+    	value2=strtok(NULL,"/");
+	}
+	value1f = my_atof(value1); // string to float
+    value2f = my_atof(value2); // string to float
+ 	float result;
+ 	result = calculate(value1f, operatorr, value2f, flagptr);
+ 	if (flag == 1){ // no error in input
+    	printf("Result = %.2f\n", result);
+    }
+ 	else{ // error in input
+    	printf("Undefined result.\n");
+	}
+	}
+	else{ // if it includes letters, special characters etc.
+		printf("Warning!!! It includes letters, special characters etc.");
+	}
+    
+}
