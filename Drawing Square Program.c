8ab94ee10a97b3eeadf696f85511850d1084dbbd
Görkem <gorkemyelken@gmail.com>
2021-01-23 17:11:15 +0300

    Assignment-3

--- Drawing Square Program.c
+++ Drawing Square Program.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main(){
-	
+
+int main(){	
 printf("*****Drawing Square Program*****\t\t\tBy Gorkem Yelken.");
 printf("Description: This program draws a square with a square hole in it.\n\n");
 
